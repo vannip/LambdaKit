78792026788d0952ed9200091fa19311474cc0fa
Vanni Parronchi <vanniparronchi@gmail.com>
2018-04-06 17:15:10 +0200

    creates xcode project for building the product adds files to target membership shares build scheme to test if carthage works

---
+++ LambdaKit Framework/LambdaKit/LambdaKit.h
@@ -0,0 +1,19 @@
+//
+//  LambdaKit.h
+//  LambdaKit
+//
+//  Created by Vanni Parronchi on 06/04/2018.
+//  Copyright © 2018 Vanni Parronchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LambdaKit.
+FOUNDATION_EXPORT double LambdaKitVersionNumber;
+
+//! Project version string for LambdaKit.
+FOUNDATION_EXPORT const unsigned char LambdaKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LambdaKit/PublicHeader.h>
+
+
